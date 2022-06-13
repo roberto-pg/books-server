c14df74f8403ba8d6dd28a12ac1278b33d006fe9
Roberto <roberto.p.gomes@hotmail.com>
2022-06-12 22:19:50 -0300

    Change docker-compose and nginx

--- src/controllers/BookController.js
+++ src/controllers/BookController.js
@@ -1,15 +1,14 @@
-const connection = require('../database/connection');
-const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
-const { validate: uuidValidate } = require('uuid');
+const connection = require("../database/connection");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+const { validate: uuidValidate } = require("uuid");
 
 module.exports = {
-
   async create(req, res) {
     const { title, author, nationality, year, read } = req.body;
     const imageurl = process.env.DIR_IMAGE + req.file.filename;
     try {
-      await connection('books').insert({
+      await connection("books").insert({
         id: uuidv4(),
         title,
         author,
@@ -25,11 +24,13 @@ module.exports = {
   },
 
   async index(req, res) {
-    const books = await connection('books').select('*').orderBy('author', 'asc');
+    const books = await connection("books")
+      .select("*")
+      .orderBy("author", "asc");
 
-    const [count] = await connection('books').count();
+    const [count] = await connection("books").count();
 
-    res.header('X-Total-Count', count['count']);
+    res.header("X-Total-Count", count["count"]);
 
     return res.json(books);
   },
@@ -38,16 +39,13 @@ module.exports = {
     const { id } = req.params;
 
     if (uuidValidate(id) == false) {
-      return res.status(400).json({ Error: 'Invalid book id' });
+      return res.status(400).json({ Error: "Invalid book id" });
     }
 
-    const book = await connection('books')
-      .select('*')
-      .where('id', id)
-      .first();
+    const book = await connection("books").select("*").where("id", id).first();
 
     if (!book) {
-      return res.status(400).json({ Error: 'Book not found in the database' });
+      return res.status(400).json({ Error: "Book not found in the database" });
     }
 
     return res.json(book);
@@ -56,24 +54,23 @@ module.exports = {
   async indexByRead(req, res) {
     var { read } = req.params;
 
-    if (read == 'all') {
-      const book = await connection('books')
-        .select('*')
-        .orderBy('author', 'asc');
+    if (read == "all") {
+      const book = await connection("books")
+        .select("*")
+        .orderBy("author", "asc");
 
-      const [count] = await connection('books').count();
-      res.header('X-Total-Count', count['count']);
+      const [count] = await connection("books").count();
+      res.header("X-Total-Count", count["count"]);
 
       return res.json(book);
     } else {
-      const book = await connection('books')
-        .select('*')
-        .where('read', read)
-        .orderBy('author', 'asc');
+      const book = await connection("books")
+        .select("*")
+        .where("read", read)
+        .orderBy("author", "asc");
 
-      const [count] = await connection('books')
-        .where('read', read).count();
-      res.header('X-Total-Count', count['count']);
+      const [count] = await connection("books").where("read", read).count();
+      res.header("X-Total-Count", count["count"]);
 
       return res.json(book);
     }
@@ -84,57 +81,50 @@ module.exports = {
     const { id } = req.params;
 
     if (uuidValidate(id) == false) {
-      return res.status(400).json({ Error: 'Invalid book id' });
+      return res.status(400).json({ Error: "Invalid book id" });
     }
 
-    const book = await connection('books').
-      select('*')
-      .where('id', id)
-      .first();
+    const book = await connection("books").select("*").where("id", id).first();
 
     if (!book) {
-      return res.status(400).json({ Error: 'Book not found in the database' });
+      return res.status(400).json({ Error: "Book not found in the database" });
     }
 
-    await connection('books')
-      .where('id', id)
-      .update({ read: read });
+    await connection("books").where("id", id).update({ read: read });
 
-    return res.json({ success: true, message: 'ok' });
+    return res.json({ success: true, message: "ok" });
   },
 
   async delete(req, res) {
     const { id } = req.params;
 
     if (uuidValidate(id) == false) {
-      return res.status(400).json({ Error: 'Invalid book id' });
+      return res.status(400).json({ Error: "Invalid book id" });
     }
 
-    const book = await connection('books')
-      .where('id', id)
-      .first();
+    const book = await connection("books").where("id", id).first();
 
     if (!book) {
-      return res.status(400).json({ Error: 'Book not found in the database' });
+      return res.status(400).json({ Error: "Book not found in the database" });
     }
 
-    const img = await connection('books')
-      .select('imageurl')
-      .where('id', id)
+    const img = await connection("books")
+      .select("imageurl")
+      .where("id", id)
       .first();
 
-    var imageName = (img.imageurl).split("\/")[3];
+    var imageName = img.imageurl.split("/")[4];
 
     fs.unlink(process.env.IMAGE_STORAGE + "/" + imageName, function (err) {
       if (err) {
         console.log("failed to delete local image:" + err);
       } else {
-        console.log('successfully deleted local image');
+        console.log("successfully deleted local image");
       }
     });
 
-    await connection('books').where('id', id).delete();
+    await connection("books").where("id", id).delete();
 
-    return res.json({ success: true, message: 'ok' });
-  }
+    return res.json({ success: true, message: "ok" });
+  },
 };
