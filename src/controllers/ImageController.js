8893b87ceb1d5cae764c4df740237922d3b06d66
Roberto <roberto.p.gomes@hotmail.com>
2022-06-13 21:15:35 -0300

    Set tsconfig

--- src/controllers/ImageController.js
+++ src/controllers/ImageController.js
@@ -1,55 +1,52 @@
-require('dotenv/config');
-const connection = require('../database/connection');
-const fs = require('fs');
-const { validate: uuidValidate } = require('uuid');
+require("dotenv/config");
+const connection = require("../database/connection");
+const fs = require("fs");
+const { validate: uuidValidate } = require("uuid");
 
 module.exports = {
-
   async updateImage(req, res) {
     const { id } = req.params;
     const imageurl = process.env.DIR_IMAGE + req.file.filename;
 
     if (uuidValidate(id) == false) {
-      return res.status(400).json({ Error: 'Invalid book id' });
+      return res.status(400).json({ Error: "Invalid book id" });
     }
 
     try {
-      const book = await connection('books')
-        .select('*')
-        .where('id', id)
+      const book = await connection("books")
+        .select("*")
+        .where("id", id)
         .first();
 
       if (!book) {
-        return res.status(400).json({ Error: 'Book not found' });
+        return res.status(400).json({ Error: "Book not found" });
       }
 
-      const img = await connection('books')
-        .select('imageurl')
-        .where('id', id)
+      const img = await connection("books")
+        .select("imageurl")
+        .where("id", id)
         .first();
 
-      var imageName = (img.imageurl).split("\/")[4];
+      var imageName = img.imageurl.split("/")[4];
 
       fs.unlink(process.env.IMAGE_STORAGE + "/" + imageName, function (err) {
         if (err) {
           console.log("failed to delete local image:" + err);
         } else {
-          console.log('successfully deleted local image');
+          console.log("successfully deleted local image");
         }
       });
 
-      await connection('books')
-        .where('id', id)
-        .update({ imageurl: imageurl });
+      await connection("books").where("id", id).update({ imageurl: imageurl });
 
-      const updatedBook = await connection('books').
-        select('*')
-        .where('id', id)
+      const updatedBook = await connection("books")
+        .select("*")
+        .where("id", id)
         .first();
 
       return res.send(updatedBook);
     } catch (err) {
       return res.status(400).json({ Error: err });
     }
-  }
+  },
 };
